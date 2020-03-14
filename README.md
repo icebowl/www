4d3892470a38b9e8525e2bbc04965ee3b4d66fa6
icebowl <icebowl1967@gmail.com>
2020-03-14 09:12:42 -0700

    AA

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# www
+# ec2html
---
+++ html/index2.php
@@ -0,0 +1,82 @@
+<html><head><title>######.tritech.io</title></head>
+<style>
+body{font-family: system-ui, sans-serif;}
+html{font-family: system-ui, sans-serif;}
+  
+pre{ 
+  background-color: #ddd; color: #000;
+  border: solid 1px; border-radius: 20px; padding : 10px;
+}
+
+a{ 	background-color:#ff0000 ;
+	color: #fff;
+	font-size:20px;
+	text-decoration:none;
+	border: solid 1px;
+	border-radius: 5px;
+	padding : 2px;
+	
+}
+
+a:hover{	background-color: #dc322f; ;
+		color:  #fdf6e3; ;
+		text-decoration:none;
+}
+</style>
+<body>
+<center>
+<a href = "data/" >data</a>&nbsp; 
+<a href = "files/" >files</a>&nbsp; 
+<a href = "http://tritech.io:8000" >python http 8000 </a>
+<br />
+<a href = "http://10.tritech.io" target ="_blank">10</a>&nbsp;
+<a href = "http://1108.tritech.io" target ="_blank">1108</a>&nbsp;
+<a href = "http://792.tritech.io" target ="_blank">792</a>&nbsp;
+<a href = "http://7.tritech.io" target ="_blank">7</a>&nbsp;
+<a href = "http://97.tritech.io" target ="_blank">97</a>&nbsp;
+<a href = "http://123.tritech.io" target ="_blank">123</a>&nbsp;
+<a href = "http://20.tritech.io" target ="_blank">20</a>&nbsp;
+<a href = "http://18.tritech.io" target ="_blank">18</a>&nbsp;
+<a href = "http://51.tritech.io" target ="_blank">51</a>&nbsp;
+<a href = "http://14.tritech.io" target ="_blank">14</a>&nbsp;
+<a href = "http://1611.tritech.io" target ="_blank">1611</a>&nbsp;
+<a href = "http://9.tritech.io" target ="_blank">9</a>&nbsp;
+<a href = "http://333.tritech.io" target ="_blank">333</a>&nbsp;
+<a href = "http://55.tritech.io" target ="_blank">55</a>&nbsp;
+<a href = "http://12.tritech.io" target ="_blank">12</a>&nbsp;
+<a href = "http://1.tritech.io" target ="_blank">1</a>&nbsp;
+<a href = "http://21.tritech.io" target ="_blank">21</a>&nbsp;
+<a href = "http://42.tritech.io" target ="_blank">42</a>&nbsp;
+<a href = "http://13.tritech.io" target ="_blank">13</a>&nbsp;
+<a href = "http://1000.tritech.io" target ="_blank">1000</a>&nbsp;
+<a href = "http://457.tritech.io" target ="_blank">457</a>&nbsp;
+<a href = "http://5.tritech.io" target ="_blank">5</a>&nbsp;
+<a href = "http://101.tritech.io" target ="_blank">101</a>&nbsp;
+<img src = "img/caz.png" height = "200px">
+</center>
+<hr />
+<pre>
+
+Save AWS security rules in the files directory.
+Save tcpdump files in the data directory. 
+
+python3 -m http.server 8000
+nohup python3 -m http.server 8000 &	
+
+Copy files to document root
+cp -fR ~/ec2html/* /var/www/html/;
+
+add this to /var/www/html/data/.htaccess
+Options +Indexes
+IndexOptions FancyIndexing FoldersFirst NameWidth=* DescriptionWidth=*
+
+add this to /var/www/html/data/.htaccess
+Options +Indexes
+IndexOptions FancyIndexing FoldersFirst NameWidth=* DescriptionWidth=*
+
+
+</pre>
+</body>
+</html>
+
+
