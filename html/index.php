0a23a01034c9b240b2bdc8cd41fc25adc3ffe562
icebowl <icebowl1967@gmail.com>
2020-03-14 09:14:31 -0700

    AA

--- html/index.php
+++ html/index.php
@@ -1,3 +1,34 @@
+<html><head><title>######.tritech.io</title></head>
+<style>
+body{font-family: system-ui, sans-serif;}
+html{font-family: system-ui, sans-serif;}
+  
+pre{ 
+  background-color: #ddd; color: #000;
+  border: solid 1px; border-radius: 20px; padding : 10px;
+}
+
+a{ 	background-color:#ff0000 ;
+	color: #fff;
+	font-size:20px;
+	text-decoration:none;
+	border: solid 1px;
+	border-radius: 5px;
+	padding : 2px;
+	
+}
+
+a:hover{	background-color: #dc322f; ;
+		color:  #fdf6e3; ;
+		text-decoration:none;
+}
+</style>
+<body>
+<center>
+<a href = "data/" >data</a>&nbsp; 
+<a href = "files/" >files</a>&nbsp; 
+<a href = "http://tritech.io:8000" >python http 8000 </a>
+<br />
 <a href = "http://10.tritech.io" target ="_blank">10</a>&nbsp;
 <a href = "http://1108.tritech.io" target ="_blank">1108</a>&nbsp;
 <a href = "http://792.tritech.io" target ="_blank">792</a>&nbsp;
@@ -21,3 +52,31 @@
 <a href = "http://457.tritech.io" target ="_blank">457</a>&nbsp;
 <a href = "http://5.tritech.io" target ="_blank">5</a>&nbsp;
 <a href = "http://101.tritech.io" target ="_blank">101</a>&nbsp;
+<img src = "img/caz.png" height = "200px">
+</center>
+<hr />
+<pre>
+
+Save AWS security rules in the files directory.
+Save tcpdump files in the data directory. 
+
+python3 -m http.server 8000
+nohup python3 -m http.server 8000 &	
+
+Copy files to document root
+cp -fR ~/ec2html/* /var/www/html/;
+
+add this to /var/www/html/data/.htaccess
+Options +Indexes
+IndexOptions FancyIndexing FoldersFirst NameWidth=* DescriptionWidth=*
+
+add this to /var/www/html/data/.htaccess
+Options +Indexes
+IndexOptions FancyIndexing FoldersFirst NameWidth=* DescriptionWidth=*
+
+
+</pre>
+</body>
+</html>
+
+
